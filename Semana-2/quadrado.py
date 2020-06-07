cd0e6760e06a6f116419e15a83bbce5fbee7c497
flaviasalisboa <flavia.lisboa@gmail.com>
2020-06-07 11:00:31 -0300

    inclusão da pasta semana-2

---
+++ Semana-2/quadrado.py
@@ -0,0 +1,6 @@
+lado = float(input("Digite o valor correspondente ao lado de um quadrado: "))
+
+perimetro = lado + lado + lado + lado;
+area = lado * lado;
+
+print("perímetro:", perimetro, "- área:", area)
