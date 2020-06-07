97bc8654cfd4b9dbce2f3be501cfef85ff17896f
flaviasalisboa <flavia.lisboa@gmail.com>
2020-06-07 15:11:16 -0300

    exercícios e comentários

--- Semana-8/funcao-remove_repetidos.py
+++ Semana-8/funcao-remove_repetidos.py
@@ -1,3 +1,6 @@
+# Semana 8 - Exercício 1 - Função remove_repetidos
+# de uma lista retira os valores repetidos
+
 def remove_repetidos(lista):
 	templista = []
 	for i in lista:
--- Semana-8/funcao-soma_elementos_lista.py
+++ Semana-8/funcao-soma_elementos_lista.py
@@ -1,3 +1,6 @@
+# Semana 8 - Exercício 2 - Função soma_elementos
+# de uma lista soma os valores
+
 def soma_elementos(lista):
 	s = 0
 	for i in lista:
