a2b493cb018ac4f58a925f7f8bbaf980071cb08f
flaviasalisboa <flavia.lisboa@gmail.com>
2020-06-07 11:10:14 -0300

    inserido comentários sobre o programa

--- Semana-2/quadrado.py
+++ Semana-2/quadrado.py
@@ -1,6 +1,9 @@
+# Semana 2 - Exercício 1 - Calcula perímetro e área de um quadrado
+# dado o valor do lado do quadrado
+
 lado = float(input("Digite o valor correspondente ao lado de um quadrado: "))
 
 perimetro = lado + lado + lado + lado;
 area = lado * lado;
 
-print("perímetro:", perimetro, "- área:", area)
+print("Perímetro:", perimetro, "- Área:", area)
